607c371b8adeeb63788c7d36d7761d043437c4f2
Ryan <ryanmccombe@gmail.com>
2015-08-05 03:33:19 +0100

    NPC Spawning

---
+++ Source/RPGBase/RPGBase.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RPGBase.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPGBase, "RPGBase" );
